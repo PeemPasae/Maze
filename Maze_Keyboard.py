b1e3a477d92d373b1533b49bc44e0eb57d799d64
Peem <6710301005@cdti.ac.th>
2025-02-05 18:08:53 +0700

    Maze_Keyboard

---
+++ Maze_Keyboard.py
@@ -0,0 +1,160 @@
+import tkinter as tk
+
+# ขนาดของแต่ละช่องในเขาวงกต
+CELL_SIZE = 50
+
+class Pos:
+    def __init__(self, y, x):
+        self.y = y
+        self.x = x
+
+class MazeGame:
+    def __init__(self, root, maps):
+        self.root = root
+        self.maps = maps
+        self.current_level = 0
+        self.load_level()
+
+
+
+        self.canvas = tk.Canvas(root, width=len(self.maze[0]) * CELL_SIZE, height=len(self.maze) * CELL_SIZE)
+        self.canvas.pack()
+        
+        self.draw_maze()
+        self.root.bind("<Up>", lambda event: self.move(-1, 0))
+        self.root.bind("<Down>", lambda event: self.move(1, 0))
+        self.root.bind("<Left>", lambda event: self.move(0, -1))
+        self.root.bind("<Right>", lambda event: self.move(0, 1))
+    
+    def load_level(self):
+        self.maze = [list(row) for row in self.maps[self.current_level]]
+        self.ply = self.find_position('P')
+        self.end = self.find_position('E')
+    
+    def find_position(self, symbol):
+        for y, row in enumerate(self.maze):
+            for x, val in enumerate(row):
+                if val == symbol:
+                    return Pos(y, x)
+        return None
+    
+    def draw_maze(self):
+        self.canvas.delete("all")
+        for y, row in enumerate(self.maze):
+            for x, cell in enumerate(row):
+                color = "white"
+                if cell == "X":
+                    color = "black"
+                elif cell == "P":
+                    color = "blue"
+                elif cell == "E":
+                    color = "green"
+                self.canvas.create_rectangle(
+                    x * CELL_SIZE, y * CELL_SIZE, 
+                    (x + 1) * CELL_SIZE, (y + 1) * CELL_SIZE, 
+                    fill=color, outline="gray"
+                )
+    
+    def move(self, dy, dx):
+        next_y, next_x = self.ply.y + dy, self.ply.x + dx
+        if 0 <= next_y < len(self.maze) and 0 <= next_x < len(self.maze[0]):
+            next_cell = self.maze[next_y][next_x]
+            if next_cell in (' ', 'E'):
+                self.maze[self.ply.y][self.ply.x] = ' '
+                self.maze[next_y][next_x] = 'P'
+                self.ply = Pos(next_y, next_x)
+                self.draw_maze()
+                if next_cell == 'E':
+                    self.next_level()
+    
+    def next_level(self):
+        self.current_level += 1
+        if self.current_level < len(self.maps):
+            self.load_level()
+            self.draw_maze()
+        else:
+            self.canvas.delete("all")
+            self.canvas.create_text(
+                len(self.maze[0]) * CELL_SIZE // 2,
+                len(self.maze) * CELL_SIZE // 2,
+                text="Congratulations! You Win!", 
+                font=("Arial", 24),
+                fill="red"
+            )
+
+if __name__ == "__main__":
+    maps = [
+    [
+        "XXXXXXXXXX",
+        "XP       X",
+        "X XXX XXX X",
+        "X   X    X",
+        "XXX X XXXX",
+        "X   X   EX",
+        "X XXXXXXXX",
+        "X        X",
+        "X XXXXXXXX",
+        "XXXXXXXXXX"
+    ],
+    [
+        "XXXXXXXXXX",
+        "XP X     X",
+        "X  X XXX X",
+        "X  X   X X",
+        "X  XXX X X",
+        "X      X X",
+        "X XXXX X X",
+        "X X    X X",
+        "X XXXXXX X",
+        "X E      X",
+        "XXXXXXXXXX"
+    ],
+    [
+        "XXXXXXXXXX",
+        "XP     X X",
+        "XXXXXX X X",
+        "X      X X",
+        "X XXXX X X",
+        "X X    X X",
+        "X X XXXX X",
+        "X X      X",
+        "X XXXXXX X",
+        "X E      X",
+        "XXXXXXXXXX"
+    ],
+    [
+        "XXXXXXXXXX",
+        "XP X     X",
+        "X  X XXX X",
+        "X  X   X X",
+        "X  XXX X X",
+        "X      X X",
+        "X XXXX X X",
+        "X X    X X",
+        "X XXXXXX X",
+        "X E      X",
+        "XXXXXXXXXX"
+    ],
+    [
+        "XXXXXXXXXX",
+        "XP       X",
+        "X XXX X XX",
+        "X   X X  X",
+        "XXX X X XX",
+        "X   X   EX",
+        "X XXXXXXXX",
+        "X   X    X",
+        "X XXXXXXXXX",
+        "XXXXXXXXXX"
+    ]
+]
+
+
+        
+        
+    
+    
+    root = tk.Tk()
+    root.title("Maze Game")
+    game = MazeGame(root, maps)
+    root.mainloop()
